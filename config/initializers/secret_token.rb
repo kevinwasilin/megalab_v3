b236c5a1314ef2be7264d6840f2e68563fa398d5
kevin <kevin@ubuntubox.(none)>
2013-04-02 10:55:47 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MegalabV3::Application.config.secret_token = 'f54835fd90c0bcb7ccfed3867f10bb7286cb3393bb715f4e7fafd17be17e03e3fb910d078b729e86f76c0e1d910088788eb5db6c54df5b64609ef00a6d4c4caf'
